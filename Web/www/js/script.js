ead772680b8676ffc7be5c00a01c0bea18ef72e4
Boris <boris.ouya@gmail.com>
2022-03-28 14:45:23 +0200

    Live verification

--- Web/www/js/script.js
+++ Web/www/js/script.js
@@ -1,66 +1,92 @@
-document.getElementById("register").addEventListener("submit", function(e) {
-    
-    let lastname = document.getElementById("lastname");
-    let firstname = document.getElementById("firstname");
-    let birthdate = document.getElementById("birthdate");
-    let username = document.getElementById("username");
-    let userpwd = document.getElementById("userpwd");
-    let useremail = document.getElementById("useremail");
-    let spans = document.getElementsByTagName("span")
+var spans = document.getElementsByTagName("span")
 
-    const emailRegex = RegExp("^[A-Za-z0-9]+.[A-Za-z0-9]+@[A-Za-z0-9]+.[A-Za-z0-9]+$")
-    const pwdRegex = RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$")
-    let birthDateString = birthdate.value.split("/").reverse().join("-");
+
+var checkUsername = false;
+var checkBirthdate = false;
+var checkUserpwd = false;
+var checkUseremail = false;
+
+document.getElementById("register").addEventListener("submit", function(e) {    
+
+    console.log(checkUseremail,checkUserpwd,checkBirthdate,checkUsername);
+    if (!(checkUserpwd && checkBirthdate && checkUsername && checkUseremail))
+    {
+        console.log(checkUseremail,checkUserpwd,checkBirthdate,checkUsername);
+        e.preventDefault()
+    }
     
-    console.log(birthDateString)
+
+});
+
+
+document.getElementById("username").addEventListener("input",function(e) {
+    let username = document.getElementById("username");
+
+    if (username.value.length < 6)
+    {
+        spans[1].innerText = "Nom d'utilisateur trop court";
+        spans[1].style.color = "red";
+        checkUsername = false;
+    }
+    else
+    {
+        spans[1].innerText = "";
+        checkUsername = true;
+    }
+
+});
+
+document.getElementById("birthdate").addEventListener("input",function() {
+    let birthdate = document.getElementById("birthdate");
+
+    let birthDateString = birthdate.value.split("/").reverse().join("-");
 
     if (isNaN(Date.parse(birthDateString)))
     {
         console.log("yolo")
         spans[0].innerHTML = "Format incorrect";
         spans[0].style.color = "red";
-        e.preventDefault()
-        return false;
+        checkBirthdate = false;
     }
     else
     {
-        console.log("yo")
         spans[0].innerText = "";
+        checkBirthdate = true;
     }
+});
 
-    if (username.value.length < 6)
-    {
-        spans[1].innerText = "Nom d'utilisateur trop court";
-        spans[1].style.color = "red";
-        e.preventDefault()
-        return false;
-    }
-    else
-    {
-        spans[1].innerText = "";
-    }
+
+document.getElementById("useremail").addEventListener("input", function() {
+
+    let useremail = document.getElementById("useremail");
+    const emailRegex = RegExp("^[A-Za-z0-9]+.[A-Za-z0-9]+@[A-Za-z0-9]+.[A-Za-z0-9]+$");
+
 
     if (!(emailRegex.test(useremail.value)))
     {
         spans[3].innerText = "Email invalide";
         spans[3].style.color = "red";
-        e.preventDefault()
-        return false;
+        checkUseremail = false ;
     }
     else{
         spans[3].innerText = "";
+        checkUseremail = true;
     }
+});
 
-    if (!pwdRegex.test(userpwd.value))
+document.getElementById("userpwd").addEventListener("input", function() {
+
+    const pwdRegex = RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$")
+
+    if (!pwdRegex.test(userpwd.value) || userpwd.value.length < 8 )
     {
         spans[2].innerText = "Mot de passe invalide";
         spans[2].style.color = "red";
-        e.preventDefault()
-        return false;
+        checkUserpwd = false;
     }
     else
     {
         spans[2].innerText = ""
+        checkUserpwd = true;
     }
-
-});
+})
